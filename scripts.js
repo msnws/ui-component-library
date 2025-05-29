447b19d9a226e3468952231a39f020e99ead2887
Mason Weis <masonweis@TheStudio.local>
2025-05-29 15:08:21 -0500

    2025-05-29 Ready to Deploy

--- scripts.js
+++ scripts.js
@@ -60,3 +60,15 @@ toggle.addEventListener("click", () => {
     toggle.innerHTML = '<i class="fa-regular fa-moon"></i>';
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+  const html = document.querySelector("html");
+  if (prefersDark.matches) {
+    html.classList.add("dark");
+    toggle.innerHTML = '<i class="fa-regular fa-sun"></i>';
+  } else {
+    html.classList.remove("dark");
+    toggle.innerHTML = '<i class="fa-regular fa-moon"></i>';
+  }
+});
